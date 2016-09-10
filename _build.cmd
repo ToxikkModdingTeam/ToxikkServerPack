c7c2f249699f6aaa4df57c9be0bc0e4632da65ae
hbeham <horst@beham.biz>
2016-09-10 06:19:10 +0200

    - updated custom map shader cache for TOXIKK v1.0
    - updated config file to changes in TOXIKK v1.0
    - latest server launcher

---
+++ _build.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+copy f:\games\steam\steamapps\common\TOXIKK\UDKGame\CookedPC\LocalShaderCache-PC-D3D-SM3.upk TOXIKK\
+if errorlevel 1 goto error
+if exist ToxikkServerPack.zip del ToxikkServerPack.zip
+if errorlevel 1 goto error
+"c:\program files\7-zip\7z.exe" a ToxikkServerPack.zip nginx TOXIKK ToxikkServerLauncher readme.txt setup.cmd steamcmd.exe
+if errorlevel 1 goto error
+goto:eof
+
+:error
+echo An error occured
+pause
