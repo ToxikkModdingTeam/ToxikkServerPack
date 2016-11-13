be898140614f8f148d5b87cf7189c01040acccfa
hbeham <horst@beham.biz>
2016-11-13 05:04:15 +0100

    - shader cache for all known custom maps + weapons based on 1.0.4
    - using HTTP downloads for workshop items which were removed from Steam

--- _build.cmd
+++ _build.cmd
@@ -5,6 +5,7 @@ copy f:\games\steam\steamapps\common\TOXIKK\UDKGame\CookedPC\LocalShaderCache-PC
 if errorlevel 1 goto error
 if exist ToxikkServerPack.zip del ToxikkServerPack.zip
 if errorlevel 1 goto error
+del ToxikkServerLauncher\MyServerConfig.ini >NUL
 "c:\program files\7-zip\7z.exe" a ToxikkServerPack.zip nginx TOXIKK ToxikkServerLauncher readme.txt setup.cmd steamcmd.exe
 if errorlevel 1 goto error
 goto:eof
